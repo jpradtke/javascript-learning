046e84d3f0036096e6a5ac9e3d6a9b1fb48895b9
jpradtke <jan.peter.radtke@googlemail.com>
2020-04-03 14:51:47 +0200

    first variables

---
+++ variables.js
@@ -0,0 +1,43 @@
+var a = 8;
+var b = "Jan";
+var c;
+console.log(a, b);
+c = "C";
+a++;
+
+console.log('Hello ' + b);
+console.log(a);
+a--;
+console.log(a);
+
+a = a + 2 // adding 2 to a -> same as a+=2
+console.log(a);
+
+a+= 2 //adding 2 to a -> same as a = a + 2
+console.log(a);
+
+a-=2; // same as a = a - 2
+console.log(a);
+
+b = b + " \"Peter\"";
+console.log(b);
+
+b = b + ' "Radtke"';
+console.log(b);
+
+var hello = "hello";
+
+console.log(hello + b);
+console.log(hello, b);
+
+var myString = "my first" + "my second"
+
+console.log(myString);
+
+var firstString = "the first string";
+console.log(firstString);
+firstString += "the second string";
+console.log(firstString);
+
+
+
