1eb973b2084431bbbb64560e9d27ba048fa08f67
jpradtke <jan.peter.radtke@googlemail.com>
2020-04-03 15:31:38 +0200

    arrays

--- variables.js
+++ variables.js
@@ -39,5 +39,47 @@ console.log(firstString);
 firstString += "the second string";
 console.log(firstString);
 
+var secondString = "second bla";
+var thirdString = "third bla";
+
+secondString += thirdString;
+console.log(secondString);
+
+firstStringLength = firstString.length;
+
+console.log(firstStringLength);
+
+var firstLetterOfFirstString = firstString[0];
+console.log(firstLetterOfFirstString);
+
+var lastLetterOfAString = firstString[firstString.length -1];
+console.log(lastLetterOfAString);
+
+var myArray = [
+    {
+        firstName: "Christina",
+        lastName: "Lerche",
+        birthDate: "19.05.1990"
+    },
+    {
+        firstName: "Jan Peter",
+        lastName: "Radtke",
+        birthDate: "24.10.1988"
+    }
+];
+
+console.log(myArray[0].firstName, myArray[0].lastName, myArray[0].birthDate);
+console.log(myArray[1].firstName, myArray[1].lastName, myArray[1].birthDate);
+
+//Function test
+function test (firstName, secondName, lastName) {
+    var result = firstName + " " + secondName + " " + lastName;
+    console.log(result);
+}
+var myFirstName = "Jan";
+var mySecondName = "Peter";
+var myLastName = "Radtke";
+test(myFirstName, mySecondName, myLastName);
+
 
 
